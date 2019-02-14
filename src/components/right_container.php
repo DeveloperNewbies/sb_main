03ebddf07bdd47e6887cd6d50c8971b9367c07b1
Alp <alparslan226@gmail.com>
2019-02-14 20:47:11 +0300

    Update right_container.php

--- src/components/right_container.php
+++ src/components/right_container.php
@@ -1,78 +1,67 @@
 <style>
-
-body{
-  font-size:20px;
-}
+    body{
+        font-size:20px;
+    }
 </style>
 <aside class="lg-side">
-   <?php if(isset($doctor_val)){?>
-       <div class="inbox-head">
-           <h4>Doktor: <?=$doctor_val[2]["name"]?>  </h4>
-           <h5>Görev Başlama Tarihi: <?=$doctor_val[2]["started_date"]?></h5>
-           <h5>Tc: <?=$doctor_val[1]?></h5>
-           <?php if(isset($old_adres[1])){ ?>
-               <h5>Seçili Olan Adres: <?=$old_adres[1]?>  </h5>
-           <?php }else{ ?>
-               <h5>Adres seçimi yapmalısınız</h5>
-           <?php }?>
-           <a href="<?=$rota."demo_dr/../report/report.php"?>?write=<?=$url?>"> <button class="btn btn-success">Çıktı Al</button></a>
-           <a href="<?=$rota."demo_dr/../report/report.php"?>?write=all"><button class="btn btn-danger">Tüm Doktorların Çıktısı</button></a>
-       <div>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="table-responsive">
-                    <table class="table  ">
-                        <thead>
-                            <tr>
+    <?php if(isset($doctor_val)){?>
+        <div class="inbox-head">
+            <div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="table-responsive">
+                            <table class="table  ">
+                                <thead>
+                                <tr>
 
-                                <th scope="col">Ad Soyad</th>
-                                <th scope="col">T.C</th>
-                                <th scope="col">Hizmet Puanı </th>
-                              <?php if(isset($old_adres[1])){ ?>
-                                <th scope="col"> Seçili Olan Adres : <?=$old_adres[1]?></th>
-                                   
-                                <?php }else{ ?>
-                                    <th scope="col"> Adres seçimi yapmalısınız </th>
-                                <?php }?>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr >
-                                
-                                <td><?=$doctor_val[2]["name"]?> </td>
-               
-                                <td><?=$doctor_val[1]?></td>
-                                <td scope="col">145236 </td>
-                                
-                            </tr>
+                                    <th scope="col">Ad Soyad</th>
+                                    <th scope="col">T.C</th>
+                                    <th scope="col">Hizmet Puanı </th>
+                                    <?php if(isset($old_adres[1])){ ?>
+                                        <th scope="col"> Seçili Olan Adres : <?=$old_adres[1]?></th>
+
+                                    <?php }else{ ?>
+                                        <th scope="col"> Adres seçimi yapmalısınız </th>
+                                    <?php }?>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr >
+
+                                    <td><?=$doctor_val[2]["name"]?> </td>
+
+                                    <td><?=$doctor_val[1]?></td>
+                                    <td scope="col">145236 </td>
+
+                                </tr>
 
 
-                        </tbody>
-                    </table>
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
+
 
                 </div>
             </div>
 
 
+            <a href="<?=$rota."pdf/index.php"?>?write=<?=$url?>"> <button class="btn btn-success">Çıktı Al</button></a>
+            <a href="<?=$rota."pdf/index.php"?>?write=all"><button class="btn btn-danger">Tüm Doktorların Çıktısı</button></a>
         </div>
-    </div>
-     
-          
-           <a href="<?=$rota."pdf/index.php"?>?write=<?=$url?>"> <button class="btn btn-success">Çıktı Al</button></a>
-           <a href="<?=$rota."pdf/index.php"?>?write=all"><button class="btn btn-danger">Tüm Doktorların Çıktısı</button></a>
-       </div>
     <?php }else{ ?>
-    <div class="inbox-head">
-        <h4>Doktor: -</h4>
-        <h5>Görev Başlama Tarihi: -</h5>
-        <h5>Tc: -</h5>
-        <?php if(isset($old_adres[1])){ ?>
-            <h5>Seçili Olan Adres: <?=$old_adres[1]?>  </h5>
-        <?php }else{ ?>
-            <h5>Adres seçimi yapmalısınız</h5>
-        <?php }?>
-        <a href="<?=$rota."demo_dr/../report/report.php"?>?write=all"><button class="btn btn-danger">Tüm Doktorların Çıktısı</button></a>
-    </div>
+        <div class="inbox-head">
+            <h4>Doktor: -</h4>
+            <h5>Görev Başlama Tarihi: -</h5>
+            <h5>Tc: -</h5>
+            <?php if(isset($old_adres[1])){ ?>
+                <h5>Seçili Olan Adres: <?=$old_adres[1]?>  </h5>
+            <?php }else{ ?>
+                <h5>Adres seçimi yapmalısınız</h5>
+            <?php }?>
+            <a href="<?=$rota."pdf/index.php"?>?write=all"><button class="btn btn-danger">Tüm Doktorların Çıktısı</button></a>
+        </div>
     <?php } ?>
     <div class="inbox-body">
         <table class="table table-inbox table-hover">
