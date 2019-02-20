33bca9245d20b1346db2c7ec1e01cf9ee740bd2a
EnesBudak <enesbudak.mdbf17@iste.edu.tr>
2019-02-20 18:00:44 +0300

    new css update
    update

--- src/components/left_container.php
+++ src/components/left_container.php
@@ -9,55 +9,45 @@
     <div class="user-head">
         <?php require_once ($rota."src/components/head_left_title.php")?>
     </div>
-<div class="col-md-1"></div>
-    <div class="btn-group btn-group-lg">
-   <td> <button class="btn btn-primary" >Sıra Bekleyenler</button>   </td>
-    <td>  <button class="btn btn-warning" >Pas  Geçenler</button>  </td>
-    <td>  <button class="btn btn-danger" >Gelmeyenler </button> </td>
-    <td>  <button class="btn btn-success" >Adres Seçenler</button>  </td>
-   
-   </div>
-    <ul class="inbox-nav inbox-divider">
-        <?php
-        foreach ($all_doctor as $value){
-            $name = $value["doctor_var"]["name"];
-            $puan = $value["doctor_var"]["started_date"];
-            $selection = $value["doctor_selection"];
-            if($selection == 0)
-                $selection = "";
-            else if($selection == 1)
-                $selection = "<span  class=\"label label-success pull-right\">Adres seçildi</span>";
-            else if($selection == 2)
-                $selection = "<span class=\"label label-warning pull-right\">Pas geçti</span>";
-            else if($selection == 3)
-                $selection = "<span class=\"label label-danger pull-right\">Gelmedi</span>";
-            $adres = $mquery->bring_adres ($value["doctor_old_place"]);
-            if($value["doctor_selection"] != 1){
-                ?>
-                <li class="<?php echo ($value['doctor_id'] == $url) ? 'active' : '' ; ?>">
-                    <a href="<?=$value['doctor_id']?>"><?=$value["must"]?> - <?=$name?> <?=$selection?> </a>
-                </li>
-            <?php }
-        }
-        ?>
-    </ul>
- <hr>
-    <ul class="inbox-nav inbox-divider">
-        <?php
-        foreach ($all_doctor as $value){
-            $name = $value["doctor_var"]["name"];
-            $puan = $value["doctor_var"]["started_date"];
-            $selection = $value["doctor_selection"];
-            $adres = $mquery->bring_adres ($value["doctor_old_place"]);
-            if($selection == 1){
-                $selection = "<span  class=\"label label-success pull-right\">Adres seçildi</span>";
-                ?>
-                <li class="<?php echo ($value['doctor_id'] == $url) ? 'active' : '' ; ?>">
-                    <a href="index.php?url=<?=$value['doctor_id']?>"><?=$value["must"]?> - <?=$name?> <?=$selection?> </a>
-                </li>
-            <?php }
-        }
-        ?>
-    </ul>
 
+    
+        <button onclick="refreshLeft('0')" class="btn btn-outline-primary" >Sıra Bekleyenler</button>   
+        <button onclick="refreshLeft('2')" class="btn btn-outline-warning" >Pas  Geçenler</button>  
+        <button onclick="refreshLeft('3')" class="btn btn-outline-danger" >Gelmeyenler </button> 
+        <button onclick="refreshLeft('1')" class="btn btn-outline-success" >Adres Seçenler</button>  
+  
+    <ul class="inbox-nav inbox-divider">
+        <?php
+        foreach ($all_doctor as $value) {
+            $name = $value["doctor_var"]["name"];
+            $puan = $value["doctor_var"]["started_date"];
+            $selection = $value["doctor_selection"];
+            if ( $_SESSION["secim"] == 0 ) {
+                if ( $selection != 1 ) {
+                    ?>
+                    <li class="<?php echo ( $value['doctor_id'] == $url ) ? 'active' : ''; ?>">
+                        <a href="<?= $value['doctor_id'] ?>"><?= $value["must"] ?> - <?= $name ?> </a>
+                    </li>
+                    <?php
+                }
+            } else if ( $_SESSION["secim"] == $selection ) {
+                ?>
+
+                <li class="<?php echo ( $value['doctor_id'] == $url ) ? 'active' : ''; ?>">
+                    <a href="<?= $value['doctor_id'] ?>"><?= $value["must"] ?> - <?= $name ?> </a>
+                </li>
+                <?php
+            }
+        }
+                 ?>
+    </ul>
 </aside>
+<script>
+    function refreshLeft(url) {
+        $.get("index.php", {"secim": url }, function (returnData, status) {
+            //alert('Status ' + status + ' The server said ' + returnData);
+            //$('#test123123').detach();
+            $('#test321').replaceWith($(returnData).find("#test321"));
+        });
+    }
+</script>
