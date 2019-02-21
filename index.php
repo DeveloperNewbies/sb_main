a8a706f6705b3200b700a85f567e1478b246597f
Mehmet Tuna <mehmet_tuna_anadolu@hotmail.com>
2019-02-21 20:17:17 +0300

    new tema

--- index.php
+++ index.php
@@ -3,49 +3,44 @@ session_start();
 $rota ="";
 require_once ( $rota . "db/query.php" );
 $mquery = new Query();
-$site_name = "localhost";
-$url = false;
-if(isset($_GET["url"])) {
 
-    $url = (is_numeric($_GET["url"])) ? $_GET["url"] : false;
-    if($url == false){
-        echo "Geçerli değer giriniz";
-        exit;
-    }
+$site_name = "localhost";
+
+    
+$url = (isset($_SESSION["url"])) ? $_SESSION["url"] : 1 ;
 
     if ( isset( $_GET["durum"] ) )
         $mquery->doctor_durum ($url , $_GET["durum"]);
-
+    
     if(isset($_GET["adres"]) && is_numeric ($_GET["adres"]))
         $mquery->doctor_adres ($url,$_GET["adres"]);
-
+    
     $doctor_variable = $mquery->bring_doctor ( $url );
-
+    
     if($doctor_variable[0]["doctor_old_place"]!=0)
         $old_adres = $mquery->bring_adres ($doctor_variable[0]["doctor_old_place"]);
-}
+        
 
-if(isset($_GET["secim"])){
-  $_SESSION["secim"] = trim($_GET["secim"]);
-} else{
-    if(!isset($_SESSION["secim"]))
-        $_SESSION["secim"] = 0;
-}
+        if(isset($_GET["secim"])){
+    $_SESSION["secim"] = trim($_GET["secim"]);
+    } else{
+        if(!isset($_SESSION["secim"]))
+            $_SESSION["secim"] = 0;
+    }
 
 $all_doctor = $mquery->all_doctor ();
 $all_adres = $mquery->all_adres ();
 
+$doctor_select_num = $mquery->doctor_var_num();
+
 ?>
 <?php require_once ($rota . "src/components/head.php");?>
 
-<body>
-<div>
-    <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
-    <div class="mail-box">
-        <?php   require_once ($rota."src/components/left_container.php") ; ?>
-        <?php   require_once ($rota."src/components/right_container.php") ; ?>
+<body id="page-top" class="sidebar-toggled">
+    <div id="wrapper">
+        <?php require_once("src/components/left_menu.php"); ?>
+        <?php require_once("src/content.php"); ?>
     </div>
-</div>
-
+  
 </body>
 </html>
